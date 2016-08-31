4670f167608dc93c82881074052883a3cdfbafbe
ianssmith <ian.s.smith0317@gmail.com>
2016-08-31 16:28:24 -0400

    cleans up name/work space

--- httpreq2.js
+++ httpreq2.js
@@ -8,13 +8,13 @@ for(var i=1;i<=10;i++){
 	if(i<10){
 		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=0'+i+'&borough=M', function(error,response,body){
 			if(!error && response.statusCode == 200){
-				fs.appendFileSync('doodle.txt', body);
+				fs.appendFileSync('data/AAhtml.txt', body);
 			}else{console.log("error");}
 		})
 	}else{
 		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone='+i+'&borough=M', function(error,response,body){
 			if(!error && response.statusCode == 200){
-				fs.appendFileSync('doodle.txt', body);
+				fs.appendFileSync('data/AAhtml.txt', body);
 			}else{console.log("error");}
 		})
 	}
