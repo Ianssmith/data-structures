0a8b888ea3aab4df0db65c431b407a95cd929cbf
ianssmith <ian.s.smith0317@gmail.com>
2016-08-31 18:18:36 -0400

    Adds functions to extract data from other 'boroughs'

--- httpreq2.js
+++ httpreq2.js
@@ -1,23 +1,43 @@
 //var async = require('async');
+//const querystring =require('querystring');
 var request = require('request');
 var fs = require('fs');
 
-//var pages = {};
+var boros = {borough:["BX","BK","M","Q","SI","N","SF","W","R","O","PD","SGU","NJ","C"]};
 
-for(var i=1;i<=10;i++){
-	if(i<10){
-		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=0'+i+'&borough=M', function(error,response,body){
-			if(!error && response.statusCode == 200){
-				fs.appendFileSync('data/AAhtml.txt', body);
-			}else{console.log("error");}
-		})
-	}else{
-		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone='+i+'&borough=M', function(error,response,body){
-			if(!error && response.statusCode == 200){
-				fs.appendFileSync('data/AAhtml.txt', body);
-			}else{console.log("error");}
-		})
+
+for(var j=0;j<=14;j++){
+	for(var i=1;i<=10;i++){
+		if(i<10){
+			request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=0'+i+'&borough='+boros.borough[j], function(error,response,body){
+				if(!error && response.statusCode == 200){
+					fs.appendFileSync('data/AAhtml_ab.txt', body);
+				}else{console.log("error");}
+			})
+		}else{
+			request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone='+i+'&borough='+boros.borough[j], function(error,response,body){
+				if(!error && response.statusCode == 200){
+					fs.appendFileSync('data/AAhtml_ab.txt', body);
+				}else{console.log("error");}
+			})
+		}
 	}
+}
+
+//var boroArray = [];
+//boroArray.push(querystring.stringify(boros.borough[i]));
+//var test = querystring.stringify(boros.borough[0]);
+//console.log(test);
+//console.log(boroArray[i]);
+
+//request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?'+querystring.stringify(boros.borough[i-1]), function(error,response,body){
+	//if(!error && response.statusCode == 200){
+		//fs.appendFileSync('data/AAhtml.txt', body);
+	//}else{console.log("error");}
+//})
+
+
+
 //console.log(pages);
 	//async.each(pages, getpages);//request().pipe(fs.createWriteStream('doodle.txt')),console.log("hi"));
 
@@ -31,4 +51,3 @@ for(var i=1;i<=10;i++){
 				//fs.appendFileSync('data/test.txt', body);
 		//}else{console.log("error");}
 	//})
-}
