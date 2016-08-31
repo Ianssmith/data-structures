fcdd483ceab3a34a4221ff5e8363d8659d3b97e1
ianssmith <ian.s.smith0317@gmail.com>
2016-08-31 16:18:09 -0400

    Found problem, appendfilesync instead of writefilesync

--- httpreq2.js
+++ httpreq2.js
@@ -1,26 +1,34 @@
-var async = require('async');
+//var async = require('async');
 var request = require('request');
 var fs = require('fs');
 
-var pages = {};
+//var pages = {};
 
 for(var i=1;i<=10;i++){
 	if(i<10){
-	pages[i] = 'http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=0'+i+'&borough=M'
+		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=0'+i+'&borough=M', function(error,response,body){
+			if(!error && response.statusCode == 200){
+				fs.appendFileSync('doodle.txt', body);
+			}else{console.log("error");}
+		})
 	}else{
-	pages[i] = 'http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone='+i+'&borough=M'
+		request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone='+i+'&borough=M', function(error,response,body){
+			if(!error && response.statusCode == 200){
+				fs.appendFileSync('doodle.txt', body);
+			}else{console.log("error");}
+		})
 	}
-}
 //console.log(pages);
-	async.each(pages, getpages);//request().pipe(fs.createWriteStream('doodle.txt')),console.log("hi"));
+	//async.each(pages, getpages);//request().pipe(fs.createWriteStream('doodle.txt')),console.log("hi"));
 
-function getpages(url){
-	request(url).pipe(fs.createWriteStream('doodle.txt'));
-}
+//function getpages(url){
+	//request(url).pipe(fs.appendFileSync('doodle.txt',url));
+//}
 
 	//.pipe(fs.createWriteStream('doodle.txt'));//, function (error, response, body) {
 		//if (!error && response.statusCode == 200) {
 			//fs.ReadStream.on('open', function(stream){
-				//fs.writeFileSync('data/test.txt', body);
+				//fs.appendFileSync('data/test.txt', body);
 		//}else{console.log("error");}
 	//})
+}
