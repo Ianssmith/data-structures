978c18e9fb8554ff93044f440f2e78bad059b650
ianssmith <ian.s.smith0317@gmail.com>
2016-09-04 14:48:21 -0400

    Changes code to save documents as seperate files

--- httpreq2.js
+++ httpreq2.js
@@ -4,16 +4,14 @@ var fs = require('fs');
 
 	for(var i=1;i<=10;i++){
 		if(i<10){
-			request('http://visualizedata.github.io/datastructures/data/m0'+i+'.html', function(error,response,body){
-				if(!error && response.statusCode == 200){
-					fs.appendFileSync('data/AAhtml_alt.txt', body);
-				}else{console.log("error");}
-			})
+			request.get('http://visualizedata.github.io/datastructures/data/m0'+i+'.html')
+				.on('error', function(err){
+					console.log(err);
+				}).pipe(fs.createWriteStream('data/AAhtml_0'+i+'.txt'));
 		}else{
-			request('http://visualizedata.github.io/datastructures/data/m'+i+'.html', function(error,response,body){
-				if(!error && response.statusCode == 200){
-					fs.appendFileSync('data/AAhtml_alt.txt', body);
-				}else{console.log("error");}
-			})
+			request.get('http://visualizedata.github.io/datastructures/data/m'+i+'.html')
+				.on('error', function(err){
+					console.log(err);
+				}).pipe(fs.createWriteStream('data/AAhtml_'+i+'.txt'));
 		}
 }
