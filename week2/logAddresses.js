426f40915c5b8258d9a6c2637109832016d81889
ianssmith <ian.s.smith0317@gmail.com>
2016-09-07 15:16:36 -0400

    complete address logging script

--- week2/logAddresses.js
+++ week2/logAddresses.js
@@ -1,27 +1,23 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-//var file = fs.readFileSync('../data/AAhtml_'+index+'.txt');
-var file = fs.readFileSync('../data/AAhtml_08.txt');
-
-var $ = cheerio.load(file);
 var reggie = new RegExp("^\s*$");
 
-
-//var res = $('td','tr');
-var tr = $('tr', 'tbody');
-
-
-tr.each(function(i,el){
-	$(el).children().first()
-	.each(function(i, el){
-		el.children.forEach(function(child, i){
-			if(child.type == "text" && child.data != reggie.test(child.data)){
-				console.log(child.data.trim());
-			}
+for(var i=1;i<=10;i++){
+	var index = i<10 ? '0'+i : i; 
+	var file = fs.readFileSync('../data/AAhtml_'+index+'.txt');
+	
+	var $ = cheerio.load(file);
+	var tr = $('tr', 'tbody');
+	
+	tr.each(function(i,el){
+		$(el).children().first()
+		.each(function(i, el){
+			el.children.forEach(function(child, i){
+				if(child.type == "text" && child.data != reggie.test(child.data)){
+					console.log(child.data.trim());
+				}
+			})
 		})
 	})
-})
-
-
-
+}
