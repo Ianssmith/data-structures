b8f8cfc0da928d938d971360315d5e086efb38aa
ianssmith <ian.s.smith0317@gmail.com>
2016-09-07 14:29:45 -0400

    updates script with address info isolated

--- week2/logAddresses.js
+++ week2/logAddresses.js
@@ -5,11 +5,23 @@ var fs = require('fs');
 var file = fs.readFileSync('../data/AAhtml_08.txt');
 
 var $ = cheerio.load(file);
+var reggie = new RegExp("^\s*$");
 
 
-var res = $('td', 'tbody').each(function(i, el){
-			console.log($(el).text());
+//var res = $('td','tr');
+var tr = $('tr', 'tbody');
+
+
+tr.each(function(i,el){
+	$(el).children().first()
+	.each(function(i, el){
+		el.children.forEach(function(child, i){
+			if(child.type == "text" && child.data != reggie.test(child.data)){
+				console.log(child.data.trim());
+			}
+		})
 	})
+})
 
 
 
