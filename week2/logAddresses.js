f9445859aeb7c9369a3707707676ef7d020bf743
ianssmith <ian.s.smith0317@gmail.com>
2016-09-12 13:07:53 -0400

    trims additional extraneous info from addresses and upload txt file of resulting log

--- week2/logAddresses.js
+++ week2/logAddresses.js
@@ -1,7 +1,7 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-var reggie = new RegExp("^\s*$");
+var blank = new RegExp("^\s*$");
 
 for(var i=1;i<=10;i++){
 	var index = i<10 ? '0'+i : i; 
@@ -14,10 +14,22 @@ for(var i=1;i<=10;i++){
 		$(el).children().first()
 		.each(function(i, el){
 			el.children.forEach(function(child, i){
-				if(child.type == "text" && child.data != reggie.test(child.data)){
-					console.log(child.data.trim());
+				if(child.type == "text" && child.data != blank.test(child.data)){
+					if(i == 6){
+						var splitter = child.data.trim().indexOf(",")
+						console.log(child.data.trim().slice(0,splitter));
+					}else{
+						var state = "NY, ";
+						console.log(state.concat(child.data.trim().slice(-5)));
+					}
 				}
 			})
 		})
 	})
 }
+
+
+/* with RegEx */
+
+//var step1 = child.data.trim().replace(/\(.*\)[,|" "]/,"");
+//console.log(step1.trim().replace(/[^0-9]*/g,""))
