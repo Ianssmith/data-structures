5d3be461bfc6375eaec16f77a322f629bf198d0b
ianssmith <ian.s.smith0317@gmail.com>
2016-10-02 12:36:56 -0400

    parses website code into objects and requests geocoded locations from google maps for each. puts into googObj.json

--- week3/geoCode.js
+++ week3/geoCode.js
@@ -1,34 +1,47 @@
+//modules
 var async = require("async");
 var fs = require("fs");
 var request = require("request")
 var cheerio = require("cheerio")
+
+//globals
 var addresses = [];
 var uris = [];
 var pages = [];
 var meetingInfo = [];
+var meetingKey = [];
 var state = ", New York, NY";
 var file = fs.readFileSync("site.html");
+var apiKey = process.env.GMAKEY;
+
 
 var blank = new RegExp("^\s*$");
 
+	/***** scrape site html into file (site.html) *****/
 			//request("http://meetings.nyintergroup.org/", function(err, response, body){
 
 	/***** scrape URI's into array *****/
 	var $ = cheerio.load(file);
 	var td = $("td[class=name]");
+
 	td.each(function(i,el){
 		$(el).children().each(function(i,child){
-			uris.push($(child).attr("href"))
+			uris.push($(child).attr("href"));
 		})
-	})
+	});
+
 
 	/***** scrape addresses into array *****/
 	var td = $("td[class=address]");
 	td.each(function(i,el){
 		addresses.push($(el).text().concat(state).replace(/ +/g,"+"));
-	})
+	});
 
-	/***** scrape meeting page code *****/
+
+	//console.log(addresses);
+
+
+	/***** scrape meeting page code and put in files (pageCode/) *****/
 		/*
 		var i=1;
 		async.eachSeries(uris, function(value,callback) {
@@ -42,38 +55,82 @@ var blank = new RegExp("^\s*$");
 		    console.log("done");
 	});
 		*/
-for(var k=1;k<uris.length;k++){
+
+/****** import page code files *****/
+
+for(var k=1;k<=uris.length;k++){
 	file = fs.readFileSync("pageCode/meeting"+k+".html");
 	var $ = cheerio.load(file);
+
+/****** seperate page info into two arrays to be combined as objects *****/
 	
-	$("dl",".col-md-4").each(function(i, el){
+	$("dt","dl").each(function(i, el){
 		if(el.data != blank.test(el.data))
-		meetingInfo.push($(el).text().trim().replace(/,/g,"").replace(/\t+/g,"").replace(/\n+/g,"___").replace(/ +/g," "));
+		meetingKey.push($(el).text().trim().replace(/,/g,"").replace(/\t+/g,"").replace(/\n+/g,"___").replace(/ +/g," "));
+		//console.log("__________________");
+	})	
+
+	$("dd","dl").each(function(i, el){
+		if(el.data != blank.test(el.data))
+		meetingInfo.push($(el).text().trim().replace(/,/g,"").replace(/\t+/g," ").replace(/\n+/g,"___").replace(/ +/g," "));
 		//console.log("__________________");
 	})	
 }
 
+for(var j=0;j<meetingKey.length;j++){
+	meetingKey[j] = meetingKey[j].split("___");
+}
 for(var j=0;j<meetingInfo.length;j++){
 	meetingInfo[j] = meetingInfo[j].split("___");
 }
 
-//for(var r=0;r<meetingInfo[r].length;r++){
-//	var temp = meetingInfo[r].slice(3,5)
-//	var temp2 = temp.toString();
-//	console.log(temp2);
-//	meetingInfo[r].splice(3,temp2)
-//	}
 
-//for(var r=0;r<meetingInfo[r].length;r++){
-	//for(var l=0;l<meetingInfo[1][l].length;l++){
-	//console.log(meetingInfo[r][l]);
-	//}
-//}
+//console.log(meetingKey)
+//console.log(meetingInfo)
 
-	console.log(meetingInfo);
+/***** combine arrays into objects and put in object array (codeobj.txt)*****/
 
+var objArray = [];
+
+var i=0;
+var place = 0;
+for(var t=0;t<uris.length;t++){
+		objArray[t] = new Object();
 	
-	
+	for(i=place;i<meetingInfo.length;i++){
+		if(meetingKey[i] != 'Last Contact Date'){
+		place++;
+			objArray[t][meetingKey[i]] = meetingInfo[i].toString();
+		}else{
+			place++;
+			break}
+	}
+}
 
+		
+for(var i=0;i<objArray.length;i++){
+	objArray[i]["Location"] = objArray[i]["Location"].replace(/\s*\(.*?\)\s*/g, "")
+}
+		
+//console.log(objArray);
 
-//})
+/***** add addresses from array scraped earlier to respective objects *****/
+
+for(var i=0;i<objArray.length;i++){
+	objArray[i].address = addresses[i];
+}
+
+//console.log(objArray);
+
+/*****request geo coords from googmaps and put into file (googObj.json)*****/
+
+async.eachSeries(objArray, function(value, callback) {
+	var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + value.address + '&key=' + apiKey;
+	request(apiRequest, function(err, resp, body) {
+		if (err) {throw err;}
+			value.latLong = JSON.parse(body).results[0].geometry.location;
+		});
+	setTimeout(callback, 1000);
+},function() {
+	    console.log((objArray));
+});
