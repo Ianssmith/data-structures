69d6eadab93a55a85fef179aada1ebe684179303
ianssmith <ian.s.smith0317@gmail.com>
2016-10-04 19:06:20 -0400

    uploads query for Tues after 7pm meeting in mongodb

---
+++ week5/qmongo.js
@@ -0,0 +1,20 @@
+var reggie = new RegExp("Tuesday 1?[7890]:[0134][05] pm")
+
+
+var mongoClient = require('mongodb').MongoClient
+var url = 'mongodb://' + process.env.mongoIP + ':27017/meetingBase'
+
+mongoClient.connect(url, function(err, db){
+	if(err){return console.dir(err);}
+
+	var collection = db.collection('meetdata')
+
+	collection.aggregate([{$match:{Time:reggie}}]).toArray(function(err, docs){
+		if(err){console.log(err)}	
+		else{
+			console.log(docs)
+			}
+			db.close();
+		})
+
+})
