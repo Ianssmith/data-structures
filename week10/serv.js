d397cc3acbd4e33c08c9b1dfc4f0e5c15b1d246e
ianssmith <ian.s.smith0317@gmail.com>
2016-11-29 03:17:10 -0500

    adds api for postgresql db

---
+++ week10/serv.js
@@ -0,0 +1,69 @@
+var models = ['crane', 'X_crane']
+
+
+//res.json(json), res.json(status, json)
+var pg = require('pg');
+
+// connection string
+
+var un = ''; // aws db username
+var pw = ''; // aws db password
+var db = ''; // aws db database name
+var ep = ''; // aws db endpoint
+var conString = "postgres://" + un + ":" + pw + "@" + ep + "/" + db;
+
+
+
+var http = require("http");
+var express = require('express');
+var app = express();
+
+//var server = http.createServer(function(req, res) {
+
+app.get('/model/:model_name',function(req,res){
+
+
+var params = req.params.model_name
+//var base = req.baseUrl
+//var q = req.query
+//var head = req.headers
+//var ip = req.ip
+//var host = req.hostname
+//var prot = req.protocol
+	
+//res.send(req.params)
+console.log(params);
+
+//parse req.params
+if(models.indexOf(params)>=0){
+
+
+//construct db query using params
+var q = "SELECT x,y,z,pitch,roll,acceleration,inclination,orientation FROM ori WHERE model = '"+params+"';"
+
+//connect and send db query
+	var dbq = pg.connect(conString, function(err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
+		}
+
+		var resp = client.query(q, function(err, result) {
+			//call `done()` to release the client back to the pool
+			done();
+
+			if (err) {
+				return console.error('error running query', err);
+			}
+			//console.log(result.rows);
+			//send db response to client
+			res.json(result.rows);
+		});
+		return resp;
+	});
+}else{
+	res.end("That model has not been made yet. You could try \""+models[Math.floor(Math.random()*(models.length))]+"\".")
+}
+
+});
+
+app.listen(8080);
