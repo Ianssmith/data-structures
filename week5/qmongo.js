5bfde81d4655bafd0d05a95a4393aefdabbb5459
ianssmith <ian.s.smith0317@gmail.com>
2016-12-19 14:59:54 -0500

    adds update to week5 query (still isnt working)

--- week5/qmongo.js
+++ week5/qmongo.js
@@ -1,7 +1,6 @@
 var datetimeStart = new Date();
 var resp = [];
 
-var reggie = new RegExp("Tuesday 1?[7890]:[0134][05] pm")
 
 
 var mongoClient = require('mongodb').MongoClient
@@ -12,14 +11,45 @@ mongoClient.connect(url, function(err, db){
 
 	var collection = db.collection('meetdata')
 
-	collection.aggregate([{$match:{Time:reggie}}]).toArray(function(err, docs){
+	collection.aggregate([
+//{"$project":{"Time":1, "Location":1, "Group":1, "Region":1, "day":1, "begin":1, "latLong":1}},
+	{$match:{$and:[
+		{day:2},{begin:{$gte:19}}
+	]}
+}
+/*
+		{ $or: [
+			{ $and:[
+				{"day":"2"},{"begin":{$gte: 19}}
+			]},
+			{ $and:[
+				{"day":"3"},{"begin":{$lte:4}}
+			]}	
+		]}	
+	},
+{$group:{ _id:{
+	latLong: "$latLong",
+	day: "$day",
+	begin: "$begin",
+},
+	meetDay : { $push: "$day"},
+	meetBegin: { $push: "$begin"}
+}	
+},
+{$group:{ _id : {
+	latLong : "$_id.latLong"},
+	meetingGroups: { $push: { timeinfo : "$_id", day : "$day", begin: "$begin"}}
+	}
+}
+*/
+		]).toArray(function(err, docs){
 		if(err){console.log(err)}	
 		else{
 			resp.push(docs)
 			}
 			db.close();
 					console.log(resp)
-			        console.log(new Date() - datetimeStart);
+			        //console.log(new Date() - datetimeStart);
 
 		})
 
